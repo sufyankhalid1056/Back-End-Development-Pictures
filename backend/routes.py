327155b4af549c9ef92b7cc21a657900a451cd18
sufyankhalid1056 <sufyankhalid1056@gmail.com>
2025-11-02 14:12:43 -0500

    almost done this one

--- backend/routes.py
+++ backend/routes.py
@@ -35,7 +35,8 @@ def count():
 ######################################################################
 @app.route("/picture", methods=["GET"])
 def get_pictures():
-    pass
+    if data:
+        return jsonify(data)
 
 ######################################################################
 # GET A PICTURE
@@ -44,7 +45,15 @@ def get_pictures():
 
 @app.route("/picture/<int:id>", methods=["GET"])
 def get_picture_by_id(id):
-    pass
+    if data:
+        count = 1
+        for x in data:
+            if count == id:
+                return jsonify(x), 200
+            else:
+                count += 1
+        
+        return {'Message': 'Not Found'}, 404
 
 
 ######################################################################
@@ -52,7 +61,34 @@ def get_picture_by_id(id):
 ######################################################################
 @app.route("/picture", methods=["POST"])
 def create_picture():
-    pass
+    rec_data = request.json
+
+    if not rec_data:
+        return {"message": "Invalid input parameter"}, 422
+
+    pic = {
+        "id": rec_data.get("id"),
+        'pic_url': rec_data.get('pic_url'),
+        'event_country': rec_data.get('event_country'),
+        'event_state': rec_data.get('event_state'),
+        'event_city': rec_data.get('event_city'),
+        'event_date': rec_data.get('event_date'),
+    }
+
+    try:
+        for x in data:
+            if x['id'] == pic['id']:
+                return {"Message": f"picture with id {pic['id']} already present"}, 302
+        data.append(pic)
+        return jsonify(pic), 201
+    except NameError:
+        return {"Nessage": "data not defined"}, 500
+
+    
+    
+    return {"Message": "Created"}, 201
+
+
 
 ######################################################################
 # UPDATE A PICTURE
@@ -61,7 +97,28 @@ def create_picture():
 
 @app.route("/picture/<int:id>", methods=["PUT"])
 def update_picture(id):
-    pass
+    rec_data = request.json
+
+    if not rec_data:
+        return {"message": "Invalid input parameter"}, 422
+
+    pic = {
+        "id": rec_data.get("id"),
+        'pic_url': rec_data.get('pic_url'),
+        'event_country': rec_data.get('event_country'),
+        'event_state': rec_data.get('event_state'),
+        'event_city': rec_data.get('event_city'),
+        'event_date': rec_data.get('event_date'),
+    }
+
+    try:
+        for x in data:
+            if x['id'] == pic['id']:
+                return jsonify(x), 200
+        return {"message": "picture not found"}, 404
+    except NameError:
+        return {"Nessage": "data not defined"}, 500
+
 
 ######################################################################
 # DELETE A PICTURE
